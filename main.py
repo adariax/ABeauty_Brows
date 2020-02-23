62b86ce5eae437f5062a52c089196781f86fa52c
Daxa <apelsinKDA@gmail.com>
2020-02-23 19:26:34 +0700

    add icon in data folder, create interface

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import sqlite3
+import sys
 
 from data.window import Ui_Form
 
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 
-# here we go
+
+class MainW(QWidget, Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.setWindowIcon(QIcon('data/ico.png'))
+        self.mode = ''
+
+
+app = QApplication(sys.argv)
+ex = MainW()
+ex.show()
+sys.exit(app.exec_())
